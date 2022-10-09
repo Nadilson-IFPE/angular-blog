7e9c7e972332ccd1ccd213a7e6a385e8df671927
Nadilson <nadilson@protonmail.com>
2022-10-09 03:58:01 -0300

    Criação de páginas

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,16 @@
+export const dataFake = [
+  {
+    id: 1,
+    photo: 'aaa',
+    title: 'Saiu novo filme',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus enim mollitia odio laborum modi quidem cumque veritatis ut facilis in! Quod illo eos quisquam temporibus porro nesciunt omnis sapiente quam!',
+  },
+  {
+    id: 2,
+    photo: 'aaa',
+    title: 'Saiu novo filme',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus enim mollitia odio laborum modi quidem cumque veritatis ut facilis in! Quod illo eos quisquam temporibus porro nesciunt omnis sapiente quam!',
+  },
+];
---
+++ src/app/pages/content/content.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css'],
+})
+export class ContentComponent implements OnInit {
+  photoCover: string =
+    'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg';
+  contentTitle: string = 'NOTÍCIA EXEMPLO';
+  contentDescription: string = 'bla bla bla';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
