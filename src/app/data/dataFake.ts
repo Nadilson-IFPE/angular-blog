1cbe1328350bb3af48a06eedac519891dc2262da
Nadilson <nadilson@protonmail.com>
2022-10-09 04:57:38 -0300

    Deixando a Content Page dinâmica

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,16 +1,37 @@
 export const dataFake = [
   {
-    id: 1,
-    photo: 'aaa',
-    title: 'Saiu novo filme',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus enim mollitia odio laborum modi quidem cumque veritatis ut facilis in! Quod illo eos quisquam temporibus porro nesciunt omnis sapiente quam!',
+    id: '1',
+    title: 'NOVO HOMEM DE FERRO EM 3D',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+    photoCover:
+      'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg',
   },
   {
-    id: 2,
-    photo: 'aaa',
-    title: 'Saiu novo filme',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus enim mollitia odio laborum modi quidem cumque veritatis ut facilis in! Quod illo eos quisquam temporibus porro nesciunt omnis sapiente quam!',
+    id: '2',
+    title: 'NOVIDADES DO HOMEM DE FERRO 5',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+    photoCover:
+      'https://a-static.mlcdn.com.br/800x560/quadro-mosaico-5-partes-para-sala-homem-de-ferro-iron-man-neyrad/cavernastoreecom/628b57c050ee11ebaf364201ac1850d0/3a5a12c4643219776df66c0e7d8e3cd5.jpeg',
+  },
+  {
+    id: '3',
+    title: 'Nova série anunciada no Disney +',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+    photoCover:
+      'https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg',
+  },
+  {
+    id: '4',
+    title: 'Novo filme do Pantera Negra será lançado em breve',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+    photoCover:
+      'http://disneyplusbrasil.com.br/wp-content/uploads/2022/09/Pantera-Negra-Wakanda-Para-Sempre.jpg',
+  },
+  {
+    id: '5',
+    title: 'Novo filme dos X-Men está em pós-produção, afirma diretor',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+    photoCover:
+      'https://midias.correiobraziliense.com.br/_midias/jpg/2022/07/22/675x450/1_xmen-26096415.jpeg?20220722171339?20220722171339',
   },
 ];
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { dataFake } from './../../data/dataFake';
 
 @Component({
   selector: 'app-content',
@@ -7,16 +8,25 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./content.component.css'],
 })
 export class ContentComponent implements OnInit {
-  photoCover: string =
-    'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg';
-  contentTitle: string = 'NOTÍCIA EXEMPLO';
-  contentDescription: string = 'bla bla bla';
+  photoCover: string = '';
+  contentTitle: string = '';
+  contentDescription: string = '';
+  private id: string | null = '0';
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((value) => {
-      console.log(value.get('id'));
+      this.id = value.get('id');
+      this.setValuesToComponents(this.id);
     });
+  }
+
+  setValuesToComponents(id: string | null) {
+    const resultado = dataFake.filter((article) => article.id === id)[0];
+
+    this.contentTitle = resultado.title;
+    this.contentDescription = resultado.description;
+    this.photoCover = resultado.photoCover;
   }
 }
